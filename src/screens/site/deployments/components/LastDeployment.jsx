a551385c55250f396b5b923a2dc7443056f533a4
Daniil Hebenton <daniil.hebenton@gmail.com>
2025-09-19 15:14:11 +0100

    ipahbfg

--- src/screens/site/deployments/components/LastDeployment.jsx
+++ src/screens/site/deployments/components/LastDeployment.jsx
@@ -12,7 +12,7 @@ export default function LastDeployment({}) {
         <Card style="last-deployment-card sites-card-fade-up f-col g24 flex9 can ">
             <p className="label">Last Deployment</p>
             <div className="f-row flex g14">
-                <img src={"https://ommbbnhzostnosfvgfjy.supabase.co/storage/v1/object/public/site-thumbnails/thumbnails/f4fe3d67-1128-4126-9796-cb4871afc53a.png"} alt="" className="site-card-thumbnail deployments-card" />
+                <img src={"https://ommbbnhzostnosfvgfjy.supabase.co/storage/v1/object/public/site-thumbnails/thumbnails/f4fe3d67-1128-4126-9796-cb4871afc53a.png"} alt="" className="site-card-thumbnail trans deployments-card" />
                 <div className="f-col flex g14">
                     <Card style="f-col g24">
                         <div className="f-row f-wrap g32">
@@ -53,21 +53,25 @@ export default function LastDeployment({}) {
                     <Card style="f-col g32">
                         <p className="label">Performance Scores</p>
                         <div className="f-row j-s-b f-wrap g20">
-                            <div className="f-col flex a-f-s g14">
-                                <p className="card-sublabel">Light House Score</p>
-                                <UsageCircleStat size={78} stroke={7} progress={80} style="deploy-card-stat"/>
+                            <div className="f-row last-deployment-last-stat flex j-s-b f-wrap g20">
+                                <div className="f-col  flex a-f-s g14">
+                                    <p className="card-sublabel">Light House Score</p>
+                                    <UsageCircleStat size={78} stroke={7} progress={80} style="deploy-card-stat"/>
+                                </div>
+                                <div className="f-col flex a-f-s g14">
+                                    <p className="card-sublabel">PSI Performance</p>
+                                    <UsageCircleStat size={78} stroke={7} progress={77} style="deploy-card-stat"/>
+                                </div>
                             </div>
-                            <div className="f-col flex a-f-s g14">
-                                <p className="card-sublabel">PSI Performance</p>
-                                <UsageCircleStat size={78} stroke={7} progress={77} style="deploy-card-stat"/>
-                            </div>
-                            <div className="f-col flex a-f-s g14">
-                                <p className="card-sublabel">PSI SEO</p>
-                                <UsageCircleStat size={78} stroke={7} progress={97} style="deploy-card-stat"/>
-                            </div>
-                            <div className="f-col flex a-f-s g14">
-                                <p className="card-sublabel">PSI Accessibility</p>
-                                <UsageCircleStat size={78} stroke={7} progress={70} style="deploy-card-stat"/>
+                            <div className="f-row flex last-deployment-last-stat j-s-b f-wrap g20">
+                                <div className="f-col  flex a-f-s g14">
+                                    <p className="card-sublabel">PSI SEO</p>
+                                    <UsageCircleStat size={78} stroke={7} progress={97} style="deploy-card-stat"/>
+                                </div>
+                                <div className="f-col flex a-f-s g14">
+                                    <p className="card-sublabel">PSI Accessibility</p>
+                                    <UsageCircleStat size={78} stroke={7} progress={70} style="deploy-card-stat"/>
+                                </div>
                             </div>
                         </div>
                     </Card>
